61afe4d487703ca71eb3fe261dc094af47d01849
aniketsabale <aniketsabale0909@gmail.com>
2024-05-19 10:48:32 +0530

    adding-changes

--- com.aniket.oops/src/Inheritance/SingleLevelInheritance.java
+++ com.aniket.oops/src/Inheritance/SingleLevelInheritance.java
@@ -3,6 +3,7 @@ class Car{
     String carType ="Hashbag";
     String getdetails(){
         String str="carType- "+carType;
+        System.out.println("calling car");
         return str;
     }
 }
@@ -11,6 +12,7 @@ class Swift extends Car {
     int price =950000;
     String getdetails(){
         String str="colour-"+colour +" and "+"price-"+price ;
+        System.out.println("calling Swift");
         return str;
     }
 
@@ -18,6 +20,10 @@ class Swift extends Car {
 public class SingleLevelInheritance {
     public static void main(String sd[]){
         Swift obj1=new Swift();
+        Car car =new Swift();
+        Car car1=new Car();
         System.out.println(obj1.getdetails());
+        System.out.println(car.getdetails());
+        System.out.println(car1.getdetails());
     }
 }
